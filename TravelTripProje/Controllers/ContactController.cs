014f02ae72c6b59fab174a4b1e41c1adf40f355e
Sedanur <119955962+sedanurpeker@users.noreply.github.com>
2025-04-02 14:56:09 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly Context _context = new Context();
+
+        // GET: İletişim formunu gösterir
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Iletisim()); // Boş bir Iletisim nesnesi ile formu aç
+        }
+
+        // POST: Kullanıcıdan gelen mesajı kaydeder
+        [HttpPost]
+        public ActionResult SendMessage(Iletisim iletisim)
+        {
+            _context.Iletisims.Add(iletisim);
+            _context.SaveChanges();
+            return View("Index", new Iletisim()); // Formu sıfırlanmış şekilde geri döndür
+        }
+    }
+}
